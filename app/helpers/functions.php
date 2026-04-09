78e39990ded88446c64a45d10f6ccdadafc200d0
Malvino <malvinogarcia2@gmail.com>
2026-04-09 16:47:41 -0600

    feat: adicionando estrutura inicial das funções de cálculo

---
+++ app/helpers/functions.php
@@ -0,0 +1,40 @@
+<?php
+
+use BcMath\Number;
+include 'app.php';
+
+    function sum($n1, $n2) {
+        return $n1 + $n2;
+    }
+
+    function sub($n1, $n2) {
+        return $n1 - $n2;
+    }
+
+    function mult($n1, $n2) {
+        return $n1 * $n2;
+    }
+
+    function div($n1, $n2) {
+        return $n1 / $n2;
+    }
+
+    function rest($n1, $n2) {
+        return $n1 % $n2;
+    }
+
+    function prct($n1, $pcrt) {
+        return ($n1 * $pcrt) / 100;
+    }
+
+    function clear($n1, $n2) {
+        
+    }
+
+    function ro($result) {
+        if ($result) 
+            ceil($result); // round cima
+        else
+            floor($result); // round cima
+    }
+?>
