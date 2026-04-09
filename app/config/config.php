d4076d500114e6c9d9f13815735528c8f91ed414
Malvino <malvinogarcia2@gmail.com>
2026-04-09 16:34:53 -0600

    feat: criando váriaveis de ambiente e adicionando a estrutura da configuração inicial para criar conexão com o banco de dados

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = $_ENV["SERVERNAME"];
+$username = $_ENV["USERNAME"];
+$password = $_ENV["PASSWORD"];
+$dbname = $_ENV["DBNAME"];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) 
+    die("Falha na conexão: $conn->connect_error");
+
+
+?>
