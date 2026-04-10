45f0246da94e0624eab662a7a04bc36df43d5f7b
Malvino <malvinogarcia2@gmail.com>
2026-04-10 16:19:17 -0600

    feat: adiciona html da calculadora e do historico

--- app/config/config.php
+++ app/config/config.php
@@ -8,6 +8,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) 
     die("Falha na conexão: $conn->connect_error");
-
-
 ?>
---
+++ app/views/hist.view.php
@@ -0,0 +1,18 @@
+<?php
+include 'config.php';
+$sql = "SELECT * FROM hist ORDER BY data_criacao DESC LIMIT 10";
+$result = $conn->query($sql);
+
+echo '<ul class="list-group list-group-flush">';
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    echo "<li class='list-group-item d-flex justify-content-between align-items-center'>";
+    echo $row['operacao'] . " = " "<strong>" . $resultado['resultado'] . "</strong>";
+    echo "</li>";
+  }
+} else {
+  echo "<li class='list-group-item'>Nenhum cálculo</li>"
+} 
+echo "</ul>";
+
+?>
